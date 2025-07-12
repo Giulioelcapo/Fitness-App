eaacd3b621397f26042ec07a566b6253b23df29b
Giulioelcapo <giulio_dambrosio@libero.it>
2025-07-12 21:08:35 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 
 import React from "react";
-import { HashRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import { AppBar, Toolbar, IconButton, Drawer, List, ListItem, ListItemText } from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
 import DashboardIcon from "@mui/icons-material/Dashboard";
